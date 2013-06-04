ad2435a78ad2d61ffa379a9bb1e33d64d25901fd
Chris Barrett <chris.d.barrett@me.com>
2013-06-04 23:45:05 +1200

    Bump version

--- emr.el
+++ emr.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2013 Chris Barrett
 
 ;; Author: Chris Barrett <chris.d.barrett@me.com>
-;; Version: 0.3.5
+;; Version: 0.3.6
 ;; Keywords: tools convenience refactoring
 ;; Package-Requires: ((s "1.3.1") (dash "1.2.0") (cl-lib "0.2") (popup "0.5.0") (emacs "24.1") (list-utils "0.3.0") (redshank "1.0.0") (paredit "24.0.0") (projectile "0.9.0"))
 ;; This file is not part of GNU Emacs.
