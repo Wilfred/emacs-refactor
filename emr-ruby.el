69562f8cc846a3a34d233603f837f4bad977d2cb
Chris Barrett <chris.d.barrett@me.com>
2014-08-18 10:27:36 +1200

    Add emr-ruby

---
+++ emr-ruby.el
@@ -0,0 +1,75 @@
+;;; emr-ruby.el --- Refactoring support for Ruby.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014 Chris Barrett
+
+;; Author: Chris Barrett <chris.d.barrett@me.com>
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Refactoring support for Ruby. Requires `Ruby Refactor`.
+;;
+;;    https://github.com/ajvargo/ruby-refactor
+;;
+
+;;; Code:
+
+(require 'emr)
+
+(emr-declare-command ruby-refactor-extract-to-method
+  :title "extract method"
+  :description nil
+  :modes enh-ruby-mode
+  :predicate (lambda ()
+               (and (require 'ruby-refactor nil t)
+                    (use-region-p))))
+
+(emr-declare-command ruby-refactor-extract-local-variable
+  :title "extract local variable"
+  :description nil
+  :modes enh-ruby-mode
+  :predicate (lambda ()
+               (and (require 'ruby-refactor nil t)
+                    (use-region-p))))
+
+(emr-declare-command ruby-refactor-extract-constant
+  :title "extract constant"
+  :description nil
+  :modes enh-ruby-mode
+  :predicate (lambda ()
+               (and (require 'ruby-refactor nil t)
+                    (use-region-p))))
+
+(emr-declare-command ruby-refactor-add-parameter
+  :title "add parameter"
+  :description nil
+  :modes enh-ruby-mode
+  :predicate (lambda ()
+               (require 'ruby-refactor nil t)))
+
+(emr-declare-command ruby-refactor-extract-to-let
+  :title "extract to let"
+  :description nil
+  :modes enh-ruby-mode
+  :predicate (lambda ()
+               (and (require 'ruby-refactor nil t)
+                    (use-region-p))))
+
+(provide 'emr-ruby)
+
+;;; emr-ruby.el ends here
